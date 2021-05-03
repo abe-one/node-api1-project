9c108e006bf857230388ac1ec7c12cfc6982b42d
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-03 16:47:17 -0500

    post user endpoint

--- api/server.js
+++ api/server.js
@@ -6,6 +6,24 @@ const UserModel = require("./users/model");
 
 server.use(express.json({ strict: true }));
 
+server.post("/api/users", async (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user",
+    });
+  } else {
+    UserModel.insert({ name, bio })
+      .then((newUser) => res.status(201).json(newUser))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          message: "There was an error while saving the user to the database",
+        });
+      });
+  }
+});
+
 server.get("/api/users", (_req, res) => {
   UserModel.find()
     .then((users) => {
